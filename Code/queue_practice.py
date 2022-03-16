40a28bdbf16ed8af1762dcd60f281f917aaf4c44
Joe Hawkins <hawkinsjwh@gmail.com>
2022-03-15 20:47:45 -0600

    Added solution to another folder

--- Code/queue_practice.py
+++ Code/queue_practice.py
@@ -71,28 +71,3 @@ add_customer_queue('Lola')
 
 print(show_customers()) #Should return the 20 customers from the dictionary and the three added above (last).
 #==================================================================
-
-
-
-
-
-
-
-
-
-#========================================================SOLUTION===========================================================
-
-# queue = []
-
-# def add_values_into_queue(dictionary):
-
-#     for key in dictionary:
-
-#         value = dictionary.get(key)
-#         queue.append(value)
-
-#     print(test_dic)
-
-# def add_customer_queue(name):
-
-#     queue.append(name)
---
+++ Code/queue_solution.py
@@ -0,0 +1,16 @@
+#========================================================SOLUTION===========================================================
+
+# queue = []
+
+# def add_values_into_queue(dictionary):
+
+#     for key in dictionary:
+
+#         value = dictionary.get(key)
+#         queue.append(value)
+
+#     print(test_dic)
+
+# def add_customer_queue(name):
+
+#     queue.append(name)

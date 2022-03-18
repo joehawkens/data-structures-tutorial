819c760c0e770395560a3939b1efe4a3925ed463
Joe Hawkins <hawkinsjwh@gmail.com>
2022-03-17 19:36:09 -0600

    changed diagram

--- Code/linked_list_example.py
+++ Code/linked_list_example.py
@@ -17,139 +17,33 @@ class linked_list():
         self.size = len(linked_list)
 
 
-    def insert_head(self, value):
+    def remove(self, value):
         """
-        Insert a new node at the front, the head, of the linked list. 
+        Remove the first node that contains the inputted value.
         """
 
-        new_node = linked_list.Node(value)  
-        
-        if self.head is None:
+        curr = self.head
+
+        while curr is not None:
+
+            if curr.data == value and curr == self.head:
+
+                self.head.next.prev = None
+                self.head = self.head.next
+                break
+
+            elif curr.data == value and curr == self.tail:
+
+                self.tail.prev.next = None
+                self.tail = self.tail.prev
+                break
+
+            elif curr.data == value:
+
+                curr.next.prev = curr.prev
+                curr.prev.next = curr.next
+                break
             
-            self.head = new_node
-            self.tail = new_node
+            else:
 
-
-        else:
-
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-
-
-    # def insert_tail(self, value):
-    #     """
-    #     Insert a new node at the back, the tail, of the linked list.
-    #     """
-
-    #     new_node = linked_list.Node(value)  
-        
-    #     if self.head is None:
-
-    #         self.head = new_node
-    #         self.tail = new_node
-
-    #     else:
-
-    #         new_node.prev = self.tail
-    #         self.tail.next = new_node
-    #         self.tail = new_node
-
-    # def remove_head(self):
-    #     """ 
-    #     Remove the first node from the linked list.
-    #     """
-
-    #     if self.head == self.tail:
-
-    #         self.head = None
-    #         self.tail = None
-
-    #     elif self.head is not None:
-
-    #         self.head.next.prev = None
-    #         self.head = self.head.next
-
-    # def remove_tail(self):
-    #     """
-    #     Remove the last node from the linked list.
-    #     """
-        
-    #     self.tail.prev.next = None
-    #     self.tail = self.tail.prev
-
-    # def remove(self, value):
-    #     """
-    #     Remove the first node that contains the inputted value.
-    #     """
-
-    #     curr = self.head
-
-    #     while curr is not None:
-
-    #         if curr.data == value and curr == self.head:
-
-    #             self.head.next.prev = None
-    #             self.head = self.head.next
-    #             break
-
-    #         elif curr.data == value and curr == self.tail:
-
-    #             self.tail.prev.next = None
-    #             self.tail = self.tail.prev
-    #             break
-
-    #         elif curr.data == value:
-
-    #             curr.next.prev = curr.prev
-    #             curr.prev.next = curr.next
-    #             break
-            
-    #         else:
-
-    #             curr = curr.next
-
-    # def replace(self, old_value, new_value):
-
-    #     """
-    #     Replaces a specified node with a new value given by the user.
-    #     """
-        
-    #     curr = self.head
-
-    #     while curr is not None:
-
-    #         if curr.data == old_value and curr == self.head:
-
-    #             curr.data = new_value
-
-    #         elif curr.data == old_value and curr == self.tail:
-
-    #             curr.data = new_value
-
-    #         elif curr.data == old_value:
-
-                
-    #             curr.data = new_value
-            
-    #         else:
-
-    #             curr = curr.next
-
-    # def __reversed__(self):
-    #     """
-    #     Iterate backward through the Linked List
-    #     """
-    #     curr = self.tail  # Start at the begining since this is a forward iteration.
-    #     while curr is not None:
-    #         yield curr.data  # Provide (yield) each item to the user
-    #         curr = curr.prev # Go forward in the linked list
-
-    # def __iter__(self):
-    #     """
-    #     Iterate foward through the Linked List
-    #     """
-    #     curr = self.head  # Start at the begining since this is a forward iteration.
-    #     while curr is not None:
-    #         yield curr.data  # Provide (yield) each item to the user
-    #         curr = curr.next # Go forward in the linked list
+                curr = curr.next
